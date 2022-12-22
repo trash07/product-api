5dc0114c840a5f20b59f6bc6fc346ff879926ae6
Lonv'ha KPETO <mmkpeto5@gmail.com>
2022-12-22 13:11:39 +0000

    add testing parameters

---
+++ src/tests/helpers/reporter.ts
@@ -0,0 +1,22 @@
+import { DisplayProcessor, SpecReporter, StacktraceOption } from 'jasmine-spec-reporter'
+// @ts-ignore
+import SuiteInfo = jasmine.SuiteInfo
+
+class CustomProcessor extends DisplayProcessor {
+    public displayJasmineStarted(info: SuiteInfo, log: string): string {
+        return `TypeScript ${log}`
+    }
+}
+
+// @ts-ignore
+jasmine.getEnv().clearReporters()
+// @ts-ignore
+jasmine.getEnv().addReporter(
+    // @ts-ignore
+    new SpecReporter({
+        spec: {
+            displayStacktrace: StacktraceOption.NONE,
+        },
+        customProcessors: [CustomProcessor],
+    })
+)
---
+++ src/tests/indexSpec.ts
@@ -0,0 +1,5 @@
+describe('test suite', () => {
+    it('should', () => {
+        expect(true).toBeTrue();
+    })
+});
