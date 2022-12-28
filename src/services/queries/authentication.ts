92e7a1a6c9f70e1f67c2075153beffbab9e231c1
Lonv'ha KPETO <mmkpeto5@gmail.com>
2022-12-28 10:34:19 +0000

    authentication test methods enhanced

--- src/services/queries/authentication.ts
+++ src/services/queries/authentication.ts
@@ -12,17 +12,21 @@ export class AuthenticationQueries {
 	 * @param password
 	 */
 	async authenticate(username: string, password: string): Promise<User | null> {
-		const conn = await client.connect();
-		const sql = `SELECT * FROM users WHERE username = ($1)`;
-		const result = await conn.query(sql, [username]);
-		if (result.rowCount > 0) {
-			const user = UserStore.convertItem(result.rows[0]);
-			const pepper = process.env.SALT;
-			const databasePassword = user.password;
-			if (bcrypt.compareSync(password + pepper, databasePassword)) {
-				return user;
+		try {
+			const conn = await client.connect();
+			const sql = `SELECT * FROM users WHERE username = ($1)`;
+			const result = await conn.query(sql, [username]);
+			if (result.rowCount > 0) {
+				const user = UserStore.convertItem(result.rows[0]);
+				const pepper = process.env.SALT;
+				const databasePassword = user.password;
+				if (bcrypt.compareSync(password + pepper, databasePassword)) {
+					return user;
+				}
 			}
+			return null;
+		} catch (e) {
+			throw new Error(`Could not authenticate user ${username}, ${e}`);
 		}
-		return null;
 	}
 }
--- src/tests/models/userSpec.ts
+++ src/tests/models/userSpec.ts
@@ -1,4 +1,5 @@
 import { UserStore } from '../../models/user';
+import { AuthenticationQueries } from '../../services/queries/authentication';
 
 describe('User model test suite', () => {
 	const store = new UserStore();
@@ -7,18 +8,44 @@ describe('User model test suite', () => {
 		expect(store.index).toBeDefined();
 	});
 
-	// Todo: write this test
-	// it('index method should find users', async () => {
-	// 	expect(false).toBeTrue();
-	// });
+	it('index method should find users', async () => {
+		const users = await store.index();
+		expect(Array.isArray(users)).toBeTrue();
+		expect(users.length).toBeGreaterThanOrEqual(0);
+	});
 
 	it('should define a show method', () => {
 		expect(store.show).toBeDefined();
+	});
+
+	it('show method should find a user by id', async () => {
+		const user = await store.create({
+			username: 'toshow',
+			password: 'showpass',
+			lastName: 'to',
+			firstName: 'show',
+		});
+		const result = await store.show(user.id as number);
+		expect(result).toEqual(user);
 	});
 
 	it('should define a create method', () => {
 		expect(store.create).toBeDefined();
 	});
 
-	// Todo: complete the specifications
+	it('create method should create a new user that can authenticate', async () => {
+		let password = 'tocreatepass';
+		const createdUser = await store.create({
+			username: 'tocreate',
+			password: password,
+			firstName: 'tocreate',
+			lastName: 'tolast',
+		});
+		const authQueries = new AuthenticationQueries();
+		const authenticationResult = await authQueries.authenticate(
+			createdUser.username,
+			password
+		);
+		expect(authenticationResult).toEqual(createdUser);
+	});
 });
